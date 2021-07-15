7d815c01f0ada9c69c684cb3b1181b8377fe5f3a
TheTaiMan <68861499+TheTaiMan@users.noreply.github.com>
2021-07-15 15:17:02 -0500

    Added search input restrictions

--- scripts/handleWordList/search.js
+++ scripts/handleWordList/search.js
@@ -69,5 +69,9 @@ input.addEventListener("input", (event) => {
   input.value =
     input.value.charAt(0).toUpperCase() + input.value.slice(1).toLowerCase();
 
+  input.value = input.value.replace(
+    /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+    ""
+  );
   searchFunction.loopCategory();
 });
