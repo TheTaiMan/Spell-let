4828032a6fa4f15369726db818df42baca5594e1
TheTaiMan <12tayenm2@gmail.com>
2026-05-06 11:29:58 -0500

    Fix localStorage key filtering to prevent external keys from breaking word list

--- scripts/handleWordList/modules/creatObj.js
+++ scripts/handleWordList/modules/creatObj.js
@@ -5,10 +5,13 @@ export const creatObj = () => {
   objectStorage = new Object();
   let orderedKeys = [];
   for (let i = 0; i < localStorage.length; i++) {
-    orderedKeys.push(localStorage.key(i));
+    let key = localStorage.key(i);
+    if (key.length === 1 && key >= "A" && key <= "Z") {
+      orderedKeys.push(key);
+    }
   }
   orderedKeys.sort();
-  for (let i = 0; i < localStorage.length; i++) {
+  for (let i = 0; i < orderedKeys.length; i++) {
     objectStorage[orderedKeys[i]] = JSON.parse(
       localStorage.getItem(orderedKeys[i])
     );
